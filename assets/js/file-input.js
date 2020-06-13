d2d1e06c444e596456a53a4950e5e1890257e8d1
Rúben Silva <ruben.silva@grama.io>
2020-06-14 00:13:00 +0100

    Add checkout payment page and file input

---
+++ assets/js/file-input.js
@@ -0,0 +1,20 @@
+var $fileInput = $('.file-input');
+var $droparea = $('.file-drop-area');
+
+
+// change inner text
+$fileInput.on('change', function() {
+  var filesCount = $(this)[0].files.length;
+  var $textContainer = $(this).prev();
+
+  if (filesCount >= 1) {
+    // if single file is selected, show file name
+    var fileName = $(this).val().split('\\').pop();
+    $textContainer.text(fileName);
+    $droparea.addClass('is-active');
+  } else {
+    // otherwise show number of files
+    $textContainer.text(filesCount + ' files selected');
+    $droparea.removeClass('is-active');
+  }
+});
