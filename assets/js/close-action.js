bb051c8f9623e12874200d68ad4e2aee86f4b935
Rúben Silva <ruben.silva@grama.io>
2020-07-31 01:27:45 +0100

    Add layout for responsive filters

--- assets/js/close-action.js
+++ assets/js/close-action.js
@@ -2,3 +2,11 @@
 $(".close-btn").click(function(){
   $(".addall-popup").removeClass('popup-open');
 });
+
+$(".close-link").click(function(){
+  $(".filters-container").removeClass('filter-open');
+});
+
+$(".filter-btn").click(function(){
+  $(".filters-container").addClass('filter-open');
+});
